4a825cff3fcffc040512cbbcd3089e3994379abf
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-10-04 12:32:23 -0700

    Class

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Student ali=new Student();
+
+            ali.Name = "Ali Gull";
+            ali.Age = 13;
+            ali.Id = 123;
+
+            ali.ShowDetails(ali);
+
+            Student azkar=new Student();
+            Console.WriteLine("________________________ \n");
+            azkar.Name = "Azkar Ul Hassan";
+            azkar.Age = 19;
+            azkar.Id = 1234;
+            azkar.ShowDetails(azkar);
+
+
+            //Code  ended here ........
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP/OOP/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace OOP
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void ShowDetails(Student ali)
+        {
+
+        Console.WriteLine("The name of Student is \nThe age of student is {1}\nThe ID of student is {2}",ali.Name,ali.Age,ali.Id);
+
+
+            
+
+        }
+
+    }
+}
+
