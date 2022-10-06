8d63349f20499d0fc4d5e2755d0eab56b3b84141
AzkarUlHassan <81754610+AzkarUlHassan@users.noreply.github.com>
2022-10-06 03:36:31 -0700

    Class of Student

---
+++ making_Class/making_Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace making_Class
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Student Ali = new Student();
+            Ali.Name = "Ali Raza";
+            Ali.Age = 29;
+            Ali.Id = 0934;
+           
+            Ali.Show(Ali);
+            Console.WriteLine("____________________________________\n");
+            Student Aneel=new Student();
+            Aneel.Name = "Aneel Ahmed";
+            Aneel.Age = 29;
+            Aneel.Id = 9798137;
+            Aneel.Show(Aneel);
+                Console.WriteLine("____________________________________\n");
+            Student Bell =new Student();
+            Bell.Name = "Bell George";
+            Bell.Age = 29;
+            Bell.Id = 9687879;
+            Bell.Show(Bell);
+            Console.WriteLine("____________________________________\n");
+            Student Center =new Student();
+            Center.Name = "Center Ahmed ";
+            Center.Age = 29;
+            Center.Id = 85674343;
+            Center.Show(Center);
+            Console.WriteLine("____________________________________\n");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ making_Class/making_Class/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace making_Class
+{
+    internal class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Show(Student Student)
+        {
+            Console.WriteLine("The Name is {0}\nThe Age is {1}\nThe Id is {2},", Student.Name, Student.Age, Student.Id);
+        }
+    }
+}
